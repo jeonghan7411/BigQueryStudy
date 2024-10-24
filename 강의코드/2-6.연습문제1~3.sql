00492270eb13ee316adec50a5fd6ff88ba72a85a
bangjeonghan <aufkrh@naver.com>
2024-10-24 22:56:21 +0900

    1~3번 문제

---
+++ 강의코드/2-6.연습문제1~3.sql
@@ -0,0 +1,24 @@
+-- 1. 포켓몬중 type2가 없는 포켓몬의 수를 작성하는 쿼리
+힌트 : 컬럼 IS NULL
+NULL : 아무것도 없는 값, 값이 존재하지 않을 때 NULL
+- 0이랑 다르고 , ""과도 다름 => 값이 없는 상태
+NULL은 다른 값과 직접 비교할 수 없음. NULL = NULL 거짓이 아님!  알수 없음!!
+
+
+SELECT
+  COUNT(id) AS cnt
+FROM
+  basic.pokemon
+WHERE 
+  type2 IS NULL
+
+
+-- 2. type2가 없는 포켓몬의 type1과 type1 의 포켓몬수를 알려주는 쿼리. 단, type1의 포켓몬 수가 큰 순으로 정렬
+SELECT
+  type1
+  , COUNT(id) AS cnt
+FROM
+  basic.pokemon
+WHERE type2 IS NULL
+GROUP BY type1
+ORDER BY cnt DESC
